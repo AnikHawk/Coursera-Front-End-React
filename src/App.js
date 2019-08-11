98537db72abd89fcf46297a782758694fc290730
AnikHawk <altamashaqueanik@gmail.com>
2019-08-12 01:34:41 +0600

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-function App () {
+function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,6 +12,7 @@ function App () {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
